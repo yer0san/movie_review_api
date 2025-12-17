8c59174025aa730c5abf9b48079afe5428501147
Yerosan Bekele <lebrondee20@gmail.com>
2025-12-17 11:36:43 +0300

    refactor register view to APIView, add permissions

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from .users import views as userViews
+
+# users
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', userViews.RegisterView.as_view(), name='register'),
     path('logout/', userViews.LogoutView.as_view(), name='logout'),
 ]
--- api/users/views.py
+++ api/users/views.py
@@ -1,15 +1,38 @@
-from rest_framework import generics
 from .serializers import RegisterSerializer
+
 from django.contrib.auth.models import User
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                "user": serializer.data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request):
         # Client should delete access & refresh tokens
         return Response({"message": "Logged out"}, status=status.HTTP_200_OK)
+
+
